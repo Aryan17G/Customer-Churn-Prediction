34ca9f21d9da8aec3b084d755cf8ee8833a07979
Aryan Gautam <143210780+Aryan17G@users.noreply.github.com>
2025-11-22 16:28:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+scaler=joblib.load("Scaler.pkl")
+model=joblib.load("model.pkl")
+st.title("Churn Prediction Application")
+st.divider()
+st.write("Please enter the value and hit the predict button for Prediction.")
+st.divider()
+age=st.number_input("Enter Age", min_value=10, max_value=100, value=30)
+tenure=st.number_input("Enter Tenure", min_value=0, max_value=130, value=10)
+monthlycharges=st.number_input("Enter Monthly Charges", min_value=30, max_value=150)
+gender=st.selectbox("Enter the Gender type",["Male","Female"])
+st.divider()
+prediction_button=st.button("Predict")
+
+if prediction_button:
+    gender_select=1 if gender == "Female" else 0
+    X= [age,gender_select,tenure,monthlycharges]
+    X1=np.array(X)
+    X_array=scaler.transform([X1])
+    prediction=model.predict(X_array)[0]
+    predicted= "Churn" if prediction==1 else "Not Churn"
+    st.balloons()
+    st.write(f"predicted:{predicted}")
+
+else:
+    st.write("Please enter the values than use prediction Button")
